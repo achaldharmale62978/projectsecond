3d8a4d9ecf06dd6f8ef0f23f482028760bc2ca86
achaldharmale62978 <achaldharmale62978@gmail.com>
2024-02-17 11:59:40 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import Solnav2 from './Component/Layout/nav2/Solnav2';
 
 function App() {
   return (
+
+    // git repository https://github.com/achaldharmale62978/projectsecond
+    // repository name projectsecond
+
+    
     <div className="App">
       <BrowserRouter>
         <Routes>
